30159eea26efc21e044f25ccec4557aa2bc00c44
Yao Shuai <ys3721@hotmail.com>
2020-05-17 23:54:41 +0800

    [proxy]Remove last proxy implements and I want write a nwe One.

---
+++ scooter-server/src/main/java/com/iceicelee/scooter/gameserver/connect/agent/HexDumpProxyServer.java
@@ -0,0 +1,12 @@
+package com.iceicelee.scooter.gameserver.connect.agent;
+
+/**
+ * @author: Yao Shuai
+ * @date: 2020/5/17 23:52
+ */
+public class HexDumpProxyServer {
+
+    public static void main(String[] args) {
+
+    }
+}
