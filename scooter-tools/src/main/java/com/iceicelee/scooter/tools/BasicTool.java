432d3c098da886392030210f8af46bfd23667b7b
Yao Shuai <ys3721@hotmail.com>
2020-09-22 20:12:12 +0800

    测试一下新的brannch吧

--- scooter-tools/src/main/java/com/iceicelee/scooter/tools/BasicTool.java
+++ scooter-tools/src/main/java/com/iceicelee/scooter/tools/BasicTool.java
@@ -3,6 +3,6 @@ package com.iceicelee.scooter.tools;
 public class BasicTool {
 	
 	public static void main(String[] args) {
-		System.out.println("Tools run!!");
+		System.out.println("新开一个branch 叫dev");
 	}
 }
