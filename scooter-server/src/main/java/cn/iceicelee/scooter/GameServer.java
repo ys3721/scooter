bd0f6a4bbc6d04dd886cc22282e32413c7c85746
he <ys3721@hotmail.com>
2020-10-05 22:37:36 +0800

    Once again，A netty server begin.

---
+++ scooter-server/src/main/java/cn/iceicelee/scooter/GameServer.java
@@ -0,0 +1,41 @@
+package cn.iceicelee.scooter;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+
+public class GameServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(boss, worker);
+        b.channel(NioServerSocketChannel.class);
+        b.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast(
+                        new HttpServerCodec(),
+                        new HttpObjectAggregator(65535),
+                        new WebSocketServerProtocolHandler("/websocket")
+                );
+            }
+        });
+        try {
+            ChannelFuture future = b.bind(1234).sync();
+            if (future.isSuccess()) {
+                System.out.println("服务器启动成功....");
+            }
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
